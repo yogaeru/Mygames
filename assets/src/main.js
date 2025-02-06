8c336a9a9e9a182944367f99d94fcf1bd63dc184
yogaeru <yogaadhip3@gmail.com>
2025-02-06 21:57:14 +0700

    Tictactoe

--- assets/src/main.js
+++ assets/src/main.js
@@ -22,15 +22,50 @@ class TicTacToe {
 
     }
 
-    makeMove(index){
-        if (this.board !== " " && !this.gameOver){
+    makeMove(index) {
+        if (this.board[index] === " " && !this.gameOver) {
             this.board[index] = this.currentPlayer;
-            
+            this.updateBoard();
+
+            if (this.checkWinner()) {
+                this.result.textContent = `Pemain ${this.currentPlayer} menang!`;
+                this.gameOver = true;
+            }
+            else if (this.board.every(cell => cell !== " ")) {
+                this.result.textContent = `Permainan Seri!!!`;
+                this.gameOver = true;
+            }
+            else {
+                this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+            }
         }
     }
+
+    updateBoard() {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell, index) => {
+            cell.textContent = this.board[index];
+            console.log(this.board);
+        })
+    }
+
+    checkWinner() {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Baris
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Kolom
+            [0, 4, 8], [2, 4, 6]             // Diagonal
+        ];
+
+        winPatterns.some(pattern => pattern.every(index => this.board[index] !== " " && this.board[index] === this.board[pattern[0]]));
+
+    };
+
+
+resetGame() {
+    this.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    this.currentPlayer = "X";
+    this.gameOver = false;
 }
 
+}
 const game = new TicTacToe();
-const elemen = document.querySelectorAll(".cell");
-
-console.log(elemen.length);
